b9c88b6a83f98b7b57d50ded70ad3ae43710f08f
danilolucena <daniloj.lucena@gmail.com>
2023-03-25 17:22:10 -0300

    Employee & Salary Employee & Commission Employee

---
+++ ConceptosPOO/ConceptosPOO/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace ConceptosPOO
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDay { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}, Birth: {BirthDay}, Hiring: {HiringDate}, Is Active: {IsActive}";
+        }
+    }
+}
--- ConceptosPOO/ConceptosPOO/Program.cs
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -5,14 +5,39 @@ using ConceptosPOO;
 Console.WriteLine("POO Conceptos!");
 Console.WriteLine("==============");
 
-try
-{
-    Console.WriteLine(new Date(2023, 22, 03));
-    Console.WriteLine(new Date(2023, 9, 23));
-    Console.WriteLine(new Date(2023, 18, 45));
-}
-catch (Exception error)
-{
+//try
+//{
+//    Console.WriteLine(new Date(2023, 22, 03));
+//    Console.WriteLine(new Date(2023, 9, 23));
+//    Console.WriteLine(new Date(2023, 18, 45));
+//}
+//catch (Exception error)
+//{
 
-    Console.WriteLine(error.Message); 
-}
+//    Console.WriteLine(error.Message); 
+//}
+
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1,
+    FirstName = "Danilo",
+    LastName = "Lucena",
+    BirthDay = new Date(1994,11,02),
+    HiringDate = new Date(2022,03,01),
+    IsActive = true,
+    Salary = 350000
+};
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2,
+    FirstName = "Giuliano",
+    LastName = "Lucena",
+    BirthDay = new Date(1995, 12, 13),
+    HiringDate = new Date(2020, 05, 11),
+    IsActive = true,
+    CommissionPercentaje = 3,
+    Sales=200000M,
+};
+Console.WriteLine(employee2);
