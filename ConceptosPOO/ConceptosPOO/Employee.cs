0bfacf68b01fcd27e95817059734ad2f201a9bfa
danilolucena <daniloj.lucena@gmail.com>
2023-03-27 17:34:28 -0300

    Interfaz y fin empleados

--- ConceptosPOO/ConceptosPOO/Employee.cs
+++ ConceptosPOO/ConceptosPOO/Employee.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConceptosPOO
 {
-    public abstract class Employee
+    public abstract class Employee : IPay
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
--- ConceptosPOO/ConceptosPOO/Program.cs
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -27,7 +27,7 @@ Employee employee1 = new SalaryEmployee()
     IsActive = true,
     Salary = 350000
 };
-Console.WriteLine(employee1);
+//Console.WriteLine(employee1);
 
 Employee employee2 = new CommissionEmployee()
 {
@@ -37,7 +37,52 @@ Employee employee2 = new CommissionEmployee()
     BirthDay = new Date(1995, 12, 13),
     HiringDate = new Date(2020, 05, 11),
     IsActive = true,
-    CommissionPercentaje = 3,
+    CommissionPercentaje = 0.03F,
     Sales=200000M,
 };
-Console.WriteLine(employee2);
+//Console.WriteLine(employee2);
+
+Employee employee3 = new HourlyEmployee()
+{
+    Id = 3,
+    FirstName = "Delfina",
+    LastName = "Lucena",
+    BirthDay = new Date(1998, 04, 22),
+    HiringDate = new Date(2021, 08, 10),
+    IsActive = true,
+    HourValue = 1100M,
+    Hours =125
+};
+//Console.WriteLine(employee3);
+
+Employee employee4 = new BaseCommisionEmployee()
+{
+    Id = 4,
+    FirstName = "Paulina",
+    LastName = "Lucena",
+    BirthDay = new Date(2004, 12, 30),
+    HiringDate = new Date(2022, 01, 11),
+    IsActive = true,
+    CommissionPercentaje = 0.015F,
+    Sales = 100000M,
+    Base = 80000
+};
+//Console.WriteLine(employee4);
+ICollection<Employee> employees = new List<Employee>()
+{
+    employee1,employee2,employee3,employee4
+};
+decimal payroll = 0;
+
+foreach(Employee e in employees)
+{
+    payroll += e.GetValueToPay();
+}
+Console.WriteLine(" ====================");
+Console.WriteLine($"Total: {payroll:C2}");
+
+Invoice invoice = new Invoice() { Description = "Iphone 13", Id = 1, Price = 300000M, Quantity = 6 };
+Invoice invoice2 = new Invoice() { Description = "Cordero", Id = 2, Price = 100000M, Quantity = 15 };
+
+Console.WriteLine(invoice);
+Console.WriteLine(invoice2);
